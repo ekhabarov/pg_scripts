ccb004ed869ca88528c44c844416e74a2a3b4968
Evgeny Khabarov <ekhabarov@dev.ms>
2016-09-12 22:22:31 +0300

    Added var and desription.

--- export_views.sh
+++ export_views.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+# Export views from PostreSQL database into separate files.
 
 file_name=""
+host=""
+db=""
 
-for f in `cat ~/listview.txt`;
+for f in `cat ~/${db}_views.txt`;
 do
   echo $f
   file_name=$(echo $f| cut -d'.' -f 2)
-  psql -qAXt -c \
-    "select 'create or replace view $file_name as ' || chr(10)|| lower(definition) || chr(10) || chr(10) || 'alter view ' || viewname || ' owner to <view_owner>;' from pg_views where schemaname || '.' || viewname = '$f'" \
-     > ~/views/$file_name.sql
+  psql -qAXt -h $host -d $db \
+    -c "select 'create or replace view $file_name as'
+    || chr(10)|| lower(definition) || chr(10) || chr(10) ||
+    'alter view ' || viewname || ' owner to ' || viewowner || ';'
+    from pg_views where schemaname || '.' || viewname = '$f'" \
+     > ~/cro/$db/sql/views/$file_name.sql
 done
 
 exit 0;
